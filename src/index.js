8aa70d91b766a6f5b59a10e160d1756d398951ff
olegov-lab <olego8760@gmail.com>
2021-01-30 15:33:06 +0300

    test: done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
-}
+    let revN;
+
+    if (n < 0) {
+    n = -n;
+    revN = n.toString();
+    revN = revN.split('');
+    revN = revN.reverse().join('');
+    return Number(revN);
+  } else if(n > 0) {
+    revN = n.toString();
+    revN = revN.split('');
+    revN = revN.reverse().join('');
+    return Number(revN);
+  }
+  }
+
